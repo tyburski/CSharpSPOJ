e35d65aabd83b8054b023cb6fc6a1a6ab4147bc8
tyburski <tyburskid7@gmail.com>
2023-12-17 14:54:10 +0100

    Dodaj pliki projektów.

---
+++ CSharpSPOJ/TEST - Life, the Universe, and Everything.cs
@@ -0,0 +1,12 @@
+﻿List<int> inputList = new List<int>();
+var input = Console.ReadLine();
+while (input != string.Empty)
+{
+    if (Convert.ToInt32(input) == 42) break;
+    inputList.Add(Convert.ToInt32(input));
+    input = Console.ReadLine();
+}
+foreach (var i in inputList)
+{
+    Console.WriteLine(i);
+}
